e3c97f1324bc5eb35e601730d6391bf1ab0dd004
ArcadiaE <119332599+ArcadiaE@users.noreply.github.com>
2023-11-19 22:56:03 +0000

    Update test_c4_2.sv

--- C4_files/test_c4_2.sv
+++ C4_files/test_c4_2.sv
@@ -26,8 +26,8 @@ module test_c4;
 timeunit 1ns;
 timeprecision 100ps;
 
-logic s, t;
-logic n_clk, rst, a;
+logic s, t, n;//add n
+logic n_clk, rst, a, c;//add c
 
 c4 c4 (.*);
 
@@ -40,73 +40,27 @@ initial
   
 initial
 
-/*
   begin
-  rst = 1;
-  a = 0;
-  #10 rst = 0;
-  #10 rst = 1;
-  #40 a = 1;
-  #60 a = 0;
-  #20 a = 1;
-  #20 a = 0;
-  #40 a = 1;
-  #20 a = 0;
-  #20 a = 1;
-  #20 $finish;
-  end
-*/
-
-/*
-begin
-        rst = 1;
-		a = 0;		
-        #20 rst = 0;
-		#20 rst = 1;  //reset to AA
-
-        
-        #20 a = 1;	// AA to AB
-        #20 a = 0;  // AB to AC
-
-        #20 a = 0;	// AC back to AA
-        #20 a = 1;  // AA to AB
-
-       
-        #20 a = 0; 	// AB to AC
-
-        
-        #20 a = 1;	// AC back to AA
-        
-        #20 a = 1;	// AA to AB
-
-        #20 a = 1;  // AB to AD
-        #20 a = 1;  // AD back to AD
-		#20 a = 0;	// AD to AA
-
-        #20 $finish;
-    end
-*/
-	
-  begin
-  c=1;
+  c=0;//add c
   rst = 1;
   a = 0;
   #20 rst = 0;
   #20 rst = 1;
-  #20 a = 1;
-  #20 a = 0;
-  #20 a = 0;
-  #20 a = 1;
-  #20 a = 0;
-  #20 a = 1;
-  #20 a = 1;
-  #20 a = 1;
-  #20 a = 1;
-  #20 a = 0;
+  #20 a = 0;c=1; //00->00
+  #20 a = 1;c=1; //00->01
+  #20 a = 0;c=1; //01->11
+  #20 a = 0;c=1; //11->00
+  #20 a = 1;c=1; //00->01
+  #20 a = 0;c=1; //01->11
+  #20 a = 1;c=1; //11->00
+  #20 a = 1;c=1; //00->01
+  #20 a = 1;c=1; //01->10
+  #20 a = 1;c=1; //10->10
+  #20 a = 0;c=1; //10->00
   
   
   
   #20 $finish;
   end
-	
+
 endmodule

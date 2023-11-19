682d2e8557f411cc704aa30aef106777e20c5191
ArcadiaE <119332599+ArcadiaE@users.noreply.github.com>
2023-11-19 22:55:13 +0000

    Update c4_3.sv

--- C4_files/c4_3.sv
+++ C4_files/c4_3.sv
@@ -21,21 +21,21 @@
 /////////////////////////////////////////////////////////////////////
 
 
-module c4 (output logic  s, t, n, SDO, input logic n_clk, rst, a, c, m, SDI);
+module c4 (output logic  s, t, n, SDO, input logic n_clk, rst, a, c, mode, SDI); //add m, SDI, SDO
 
 logic s_plus, t_plus, s_bar, t_bar, n_bar, clk;
 
 assign clk = ~n_clk;
 
 next_state n0 (.*);
-
 scan_dff d0 (.q(s), .qbar(s_bar), .clk(clk), .rst(rst), .d(s_plus), .mode(m), .scan_in(t));
 scan_dff d1 (.q(t), .qbar(t_bar), .clk(clk), .rst(rst), .d(t_plus), .mode(m), .scan_in(SDI));
-output_reg o1 (.*);
+output_reg o1 (.*);//calculate n
 
 always_comb begin
     SDO = s;
   end
-
+ //add the scan
 endmodule
+
 

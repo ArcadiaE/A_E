3441fdcc2c5767194c26da379883ce381e1860c9
ArcadiaE <119332599+ArcadiaE@users.noreply.github.com>
2023-11-20 11:54:22 +0000

    Update c4_3.sv

--- C4_files/c4_3.sv
+++ C4_files/c4_3.sv
@@ -21,21 +21,30 @@
 /////////////////////////////////////////////////////////////////////
 
 
-module c4 (output logic  s, t, n, SDO, input logic n_clk, rst, a, c, mode, SDI); //add m, SDI, SDO
+module c4 (output logic  s, t, n, SDO, input logic n_clk, rst, a, c, m, SDI);
 
 logic s_plus, t_plus, s_bar, t_bar, n_bar, clk;
 
 assign clk = ~n_clk;
 
-next_state n0 (.*);
-scan_dff d0 (.q(s), .qbar(s_bar), .clk(clk), .rst(rst), .d(s_plus), .mode(m), .scan_in(t));
-scan_dff d1 (.q(t), .qbar(t_bar), .clk(clk), .rst(rst), .d(t_plus), .mode(m), .scan_in(SDI));
-output_reg o1 (.*);//calculate n
+always @(*)
+if (m) assign SDO = n;
+else SDO = 0;
 
+next_state n0 (.*);
+
+scan_dff d0 (.q(s), .qbar(s_bar), .clk(clk), .rst(rst), .d(s_plus), .mode(m), .scan_in(SDI));
+scan_dff d1 (.q(t), .qbar(t_bar), .clk(clk), .rst(rst), .d(t_plus), .mode(m), .scan_in(s));
+output_reg o1 (.n(n), .clk(clk), . rst(rst), .s(s), .t(t), .c(c), .mode(m), .scan_in(t));
+
+/*
 always_comb begin
     SDO = s;
   end
- //add the scan
+*/
+
 endmodule
+
+
 
 

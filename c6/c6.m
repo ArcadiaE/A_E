73baa2ffbe64819b8d6c6b51017d1009a9afb2ae
ArcadiaE <119332599+ArcadiaE@users.noreply.github.com>
2023-12-08 22:55:56 +0000

    Delete c6 directory

--- c6/c6.m
+++
@@ -1,7 +0,0 @@
-clear all, close all
-T = 1; ts = 0.01; % time horizon and discretization time
-t = 0:ts:T; % time vector
-phi = 0;
-u = sin(2*pi*1.1*t + phi); % signal
-figure, hold on, stem(t,u,'k')
-title('Signal'), xlabel('time, t'), ylabel('u(t)')
--- c6/c63.m
+++
@@ -1,6 +0,0 @@
-u5 = sin(2*pi*(2*t)); % time scale by constant > 1
-u6 = sin(2*pi*(1/2*t)); % time scale by constant < 1
-figure, hold on
-stem(t,u,'k'), stem(t,u5,'r'), stem(t,u6,'b')
-title('Original signal and its time scaled versions')
-xlabel('time, t'), ylabel('u(t), u5(t), u6(t)')

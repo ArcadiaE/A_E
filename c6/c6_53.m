bb4a5e2aa65588393117331cab565915dfb5624f
ArcadiaE <119332599+ArcadiaE@users.noreply.github.com>
2023-12-11 12:26:20 +0000

    Update c6_53.m

--- c6/c6_53.m
+++ c6/c6_53.m
@@ -22,4 +22,18 @@ for N = N_values
     pdf_values = pdf_values * (max(x) - min(x)) / sum(pdf_values); % Scale PDF to match the histogram
     plot(edges, pdf_values * N, 'r', 'LineWidth', 2); % Plotting the theoretical PDF
     hold off;
+
+        % Calculate and plot the empirical CDF
+    figure;
+    [cdf_values, cdf_edges] = ecdf(x); % Empirical CDF
+    stairs(cdf_edges, cdf_values, 'b', 'LineWidth', 2); % Plotting the empirical CDF
+    hold on;
+    theoretical_cdf = normcdf(edges, 0, 1); % Theoretical CDF
+    plot(edges, theoretical_cdf, 'r--', 'LineWidth', 2); % Plotting the theoretical CDF
+    title(['CDF of Gaussian RV with N = ', num2str(N)]);
+    xlabel('Value');
+    ylabel('Cumulative Probability');
+    legend('Empirical CDF', 'Theoretical CDF');
+    hold off;
+end
 end
